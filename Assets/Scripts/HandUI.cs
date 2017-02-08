931d3c7044c3c828c0fcc418e10517f300e2a45e
Mathias Siig Nørregaard <mathias.norregaard@futurice.com>
2017-02-08 14:34:00 +0200

    Super basic UI

---
+++ Assets/Scripts/HandUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandUI : MonoBehaviour
+{
+    /// <summary>
+    /// Object used to figure out when to introduce UI
+    /// </summary>
+    public Transform Palm;
+    /// <summary>
+    /// Menu to show/hide
+    /// </summary>
+    public GameObject Menu; 
+    public float RotationThreshold = 200f;
+
+    private bool MenuShown = false;
+	
+	// Update is called once per frame
+	void Update () {
+        RotateToShowMenu(Palm);
+	}
+
+    private void RotateToShowMenu(Transform t)
+    {
+        if (t.localRotation.eulerAngles.z >= RotationThreshold && !MenuShown)
+        {
+            ShowMenu();
+            MenuShown = true;
+            return;
+        }
+        else if (t.localRotation.eulerAngles.z < RotationThreshold && MenuShown)
+        {
+            HideMenu();
+            MenuShown = false;
+        }
+    }
+
+    private void ShowMenu()
+    {
+        Menu.SetActive(true);
+        Debug.Log("Setting active");
+    }
+
+    private void HideMenu()
+    {
+        Menu.SetActive(false);
+    }
+}
