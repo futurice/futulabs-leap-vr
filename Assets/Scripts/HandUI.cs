ef69209c17d18f4f39557145c1cba1842e24c572
Mathias Siig Nørregaard <mathias.norregaard@futurice.com>
2017-02-09 11:52:54 +0200

    Hand pool?

--- Assets/Scripts/HandUI.cs
+++ Assets/Scripts/HandUI.cs
@@ -1,60 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class HandUI : MonoBehaviour
+namespace Futulabs
 {
-    /// <summary>
-    /// Object used to figure out when to introduce UI
-    /// </summary>
-    public Transform Palm;
-    /// <summary>
-    /// Menu to show/hide
-    /// </summary>
-    public GameObject Menu; 
-    public float RotationThreshold = 200f;
-    public float TimeThresHold = 1f;
-    private float TimeDT = 0;
-
-    private bool MenuShown = false;
-	
-	// Update is called once per frame
-	void Update () {
-        RotateToShowMenu(Palm);
-	}
-
-    private void RotateToShowMenu(Transform t)
+    public class HandUI : MonoBehaviour
     {
-        Vector3 rot = t.localRotation.eulerAngles;
-        if (rot.z >= RotationThreshold && !MenuShown && rot.z >= 0)
+        /// <summary>
+        /// Object used to figure out when to introduce UI
+        /// </summary>
+        public Transform Palm;
+        /// <summary>
+        /// Menu to show/hide
+        /// </summary>
+        public GameObject Menu;
+        public float RotationThreshold = 200f;
+        public float TimeThresHold = 1f;
+        private float TimeDT = 0;
+
+        private bool MenuShown = false;
+
+        // Update is called once per frame
+        void Update()
         {
-            TimeDT += Time.deltaTime;
-            if (TimeDT >= TimeThresHold)
+            RotateToShowMenu(Palm);
+        }
+
+        private void RotateToShowMenu(Transform t)
+        {
+            Vector3 rot = t.localRotation.eulerAngles;
+            if (rot.z >= RotationThreshold && !MenuShown && rot.z >= 0)
             {
-                ShowMenu();
-                MenuShown = true;
-                TimeDT = 0;
+                TimeDT += Time.deltaTime;
+                if (TimeDT >= TimeThresHold)
+                {
+                    ShowMenu();
+                    MenuShown = true;
+                    TimeDT = 0;
+                }
+            }
+            if (rot.z < RotationThreshold && MenuShown)
+            {
+                TimeDT += Time.deltaTime;
+                if (TimeDT >= TimeThresHold)
+                {
+                    HideMenu();
+                    MenuShown = false;
+                    TimeDT = 0;
+                }
             }
         }
-        if (rot.z < RotationThreshold && MenuShown)
+
+        private void ShowMenu()
         {
-            TimeDT += Time.deltaTime;
-            if (TimeDT >= TimeThresHold)
-            {
-                HideMenu();
-                MenuShown = false;
-                TimeDT = 0;
-            }
+            Menu.SetActive(true);
         }
-    }
 
-    private void ShowMenu()
-    {
-        Menu.SetActive(true);
-    }
-
-    private void HideMenu()
-    {
-        Menu.SetActive(false);
+        private void HideMenu()
+        {
+            Menu.SetActive(false);
+        }
     }
 }
