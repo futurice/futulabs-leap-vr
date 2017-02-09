6670c8d9148dfb66cbd12facafb154723d71afb8
Mathias Siig Nørregaard <mathias.norregaard@futurice.com>
2017-02-09 16:33:19 +0200

    Added button sounds

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,45 +5,57 @@ using UnityEngine;
 namespace Futulabs
 {
 
-public class AudioManager : Singleton<AudioManager>
-{
-    [Header("AudioManager")]
-    public List<GameAudioClip> _gameAudioClips;
-
-    private Dictionary<GameAudioClipType, AudioClip> _audioClipDictionary;
-
-    private Dictionary<GameAudioClipType, AudioClip> AudioClipDictionary
+    public class AudioManager : Singleton<AudioManager>
     {
-        get
-        {
-            try
-            {
-                if (_audioClipDictionary == null)
-                {
-                    _audioClipDictionary = new Dictionary<GameAudioClipType, AudioClip>();
-                    int numGameAudioClips = _gameAudioClips.Count;
+        [Header("AudioManager")]
+        [SerializeField]
+        private AudioSource _effectAudioSource;
+        public List<GameAudioClip> _gameAudioClips;
 
-                    for (int i = 0; i < numGameAudioClips; ++i)
+        private Dictionary<GameAudioClipType, AudioClip> _audioClipDictionary;
+
+        private Dictionary<GameAudioClipType, AudioClip> AudioClipDictionary
+        {
+            get
+            {
+                try
+                {
+                    if (_audioClipDictionary == null)
                     {
-                        _audioClipDictionary.Add(_gameAudioClips[i].audioClipType, _gameAudioClips[i].audioClip);
+                        _audioClipDictionary = new Dictionary<GameAudioClipType, AudioClip>();
+                        int numGameAudioClips = _gameAudioClips.Count;
+
+                        for (int i = 0; i < numGameAudioClips; ++i)
+                        {
+                            _audioClipDictionary.Add(_gameAudioClips[i].audioClipType, _gameAudioClips[i].audioClip);
+                        }
                     }
                 }
-            }
-            catch (System.Exception e)
-            {
-                Debug.LogErrorFormat("AudioManager AudioClipDictionary: Error while creating AudioClipDictionary - {0}", e.Message);
-            }
+                catch (System.Exception e)
+                {
+                    Debug.LogErrorFormat("AudioManager AudioClipDictionary: Error while creating AudioClipDictionary - {0}", e.Message);
+                }
 
-            return _audioClipDictionary;
+                return _audioClipDictionary;
+            }
         }
+
+        public AudioClip GetAudioClip(GameAudioClipType type)
+        {
+            AudioClip audioClip = null;
+            AudioClipDictionary.TryGetValue(type, out audioClip);
+            return audioClip;
         }
-    
-    public AudioClip GetAudioClip(GameAudioClipType type)
-    {
-        AudioClip audioClip = null;
-        AudioClipDictionary.TryGetValue(type, out audioClip);
-        return audioClip;
+
+        public void PlayAudioClip(int type)
+        {
+            PlayAudioClip((GameAudioClipType)type);
+        }
+
+        public void PlayAudioClip(GameAudioClipType type)
+        {
+            _effectAudioSource.PlayOneShot(GetAudioClip(type));
+        }
     }
-}
 
 }
