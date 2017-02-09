6d8c184186719f3a4ac4a02cfd7c73ac518d9eca
Mathias Siig Nørregaard <mathias.norregaard@futurice.com>
2017-02-09 15:59:58 +0200

    Reset button added

---
+++ Assets/Scripts/UI/TimeButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+namespace Futulabs
+{
+    public class TimeButton : MonoBehaviour
+    {
+        public float TimeToActivate = 3f;
+        private float dt = 0;
+
+        public GameManager game;
+        public Image RadialImage;
+        private bool isDown;
+        public void ButtonDown()
+        {
+            isDown = true;
+        }
+
+        public void ButtonUp()
+        {
+            isDown = false;
+            RadialImage.DOFillAmount(0, 0.5f).SetEase(Ease.OutExpo);
+        }
+
+        private void Update()
+        {
+            if (isDown)
+                dt += Time.deltaTime;
+            else
+                dt = 0;
+            if (dt == 0)
+                return;
+            float percentage = dt / TimeToActivate;
+            RadialImage.fillAmount = percentage;
+            if (dt >= TimeToActivate)
+            {
+                Activate();
+            }
+        }
+
+        private void Activate()
+        {
+            game.ResetGame();
+        }
+    }
+}
