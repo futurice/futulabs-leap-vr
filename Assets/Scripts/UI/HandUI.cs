3c54e786c76de0b8cf94d039211db519b562bced
Mathias Siig Nørregaard <mathias.norregaard@futurice.com>
2017-02-15 11:19:49 +0200

    Fixed the Hand UI

--- Assets/Scripts/UI/HandUI.cs
+++ Assets/Scripts/UI/HandUI.cs
@@ -6,92 +6,56 @@ namespace Futulabs
 {
     public class HandUI : MonoBehaviour
     {
-        /// <summary>
-        /// Object used to figure out when to introduce UI
-        /// </summary>
-        public Transform Palm;
-        /// <summary>
-        /// Menu to show/hide
-        /// </summary>
         public GameObject Menu;
         public CanvasGroup canvasGroup;
         public float RotationThreshold = 200f;
         public float TimeThresHold = 1f;
-        private float TimeDT = 0;
 
         public Vector3 StartPos;
         public Vector3 EndPos;
         public float FadeTime = 0.25f;
 
-        public Transform MenuBox;
+        private bool _menuShown = false;
+        private float _timeDT = 0;
+        private Tweener _moveTween;
+        private Tweener _alphaTween;
 
-
-        private bool MenuShown = false;
-
-        void Start()
+        private void Awake()
         {
-            //Menu.transform.localPosition = StartPos;
-            //canvasGroup.alpha = 0;
+            HideMenu();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void KillMenuTweens()
         {
-            RotateToShowMenu(Palm);
-        }
-
-        private bool CheckToShowMenu()
-        {
-            Debug.DrawRay(Camera.main.transform.position,
-                (MenuBox.position - Camera.main.transform.position).normalized);
-            RaycastHit hit;
-            Physics.Raycast(new Ray(Camera.main.transform.position,
-                (MenuBox.position - Camera.main.transform.position).normalized),out hit);
-            return false;
-        }
-
-        private void RotateToShowMenu(Transform t)
-        {
-            /*
-            bool CheckShow = CheckToShowMenu();
-            Debug.Log(CheckShow);
-
-            if (CheckShow && !MenuShown)
+            if (_moveTween != null)
             {
-                ShowMenu();
-                MenuShown = true;
+                _moveTween.Kill();
             }
 
-            if (CheckShow && MenuShown)
+            if (_alphaTween != null)
             {
-                HideMenu();
-                MenuShown = false;
-            }*/
+                _alphaTween.Kill();
+            }
         }
 
-        Tweener moveTween;
-        Tweener alphaTween;
-
-        private void ShowMenu()
+        public void ShowMenu()
         {
-            if (moveTween != null)
-                moveTween.Kill();
-            if (alphaTween != null)
-                alphaTween.Kill();
-            moveTween = Menu.transform.DOLocalMove(EndPos, FadeTime).SetEase(Ease.OutExpo);
-            alphaTween = canvasGroup.DOFade(1, FadeTime).SetEase(Ease.OutExpo);
+            KillMenuTweens();
+
+            _menuShown = true;
+            _moveTween = Menu.transform.DOLocalMove(EndPos, FadeTime).SetEase(Ease.OutExpo);
+            _alphaTween = canvasGroup.DOFade(1, FadeTime).SetEase(Ease.OutExpo);
             canvasGroup.interactable = true;
         }
 
-        private void HideMenu()
+        public void HideMenu()
         {
-            if (moveTween != null)
-                moveTween.Kill();
-            if (alphaTween != null)
-                alphaTween.Kill();
-            moveTween = Menu.transform.DOLocalMove(StartPos, FadeTime / 2).SetEase(Ease.OutExpo);
+            KillMenuTweens();
+
+            _menuShown = false;
+            _moveTween = Menu.transform.DOLocalMove(StartPos, FadeTime / 2).SetEase(Ease.OutExpo);
             canvasGroup.interactable = false;
-            alphaTween = canvasGroup.DOFade(0, FadeTime / 2).SetEase(Ease.OutExpo);
+            _alphaTween = canvasGroup.DOFade(0, FadeTime / 2).SetEase(Ease.OutExpo);
         }
     }
 }
