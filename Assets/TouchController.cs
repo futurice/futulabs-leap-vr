5c4741726d2a11e8becca062c9a0196bea3d533e
Joonas Nissinen <joonas.nissinen@futurice.com>
2017-02-28 13:12:27 +0200

    Joonas added foot trackers. Changed location of hoop and mirror

---
+++ Assets/TouchController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Futulabs
+{ 
+    public class TouchController : MonoBehaviour
+    {
+        [SerializeField]
+        OVRInput.Controller _controller;
+
+        private void Update()
+        {
+            Vector3 localPosition = OVRInput.GetLocalControllerPosition(_controller);
+            Quaternion localRotation = OVRInput.GetLocalControllerRotation(_controller);
+
+            transform.localPosition = localPosition;
+            transform.localRotation = localRotation;
+        }
+    }
+}
