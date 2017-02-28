d1d60fe71e90b36ef30169c4749911f5dfed5bc9
Joonas Nissinen <joonas.nissinen@futurice.com>
2017-02-28 13:44:23 +0200

    Added third person camera

---
+++ Assets/Scripts/Managers/ThirdPersonManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Futulabs
+{
+    public class ThirdPersonManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _target;
+        [SerializeField]
+        private float _yOffset;
+        [SerializeField]
+        private float _radius = 2;
+        private float _theta;
+        [SerializeField]
+        private float _rotationSpeed;
+
+        private void Update()
+        {
+            float input = Input.GetAxis("Horizontal");
+            _theta += input* _rotationSpeed;
+            float x = 0 + _radius * Mathf.Cos(_theta * Mathf.PI / 180);
+            float z = 0 + _radius * Mathf.Sin(_theta * Mathf.PI / 180);
+            transform.position = new Vector3(x, _yOffset, z);
+            transform.LookAt(_target);
+        }
+
+
+
+    }
+}
