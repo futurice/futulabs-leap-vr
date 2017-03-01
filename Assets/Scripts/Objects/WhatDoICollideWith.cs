8c5640733abe96ea9efb3577a97e0e0fd3fdd425
Joonas Nissinen <joonas.nissinen@futurice.com>
2017-03-01 12:39:57 +0200

    AudioManager reassigned cut sounds

---
+++ Assets/Scripts/Objects/WhatDoICollideWith.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatDoICollideWith : MonoBehaviour
+{
+
+    private HashSet<Collider> colliders = new HashSet<Collider>();
+    public List<string> colliderNames = new List<string>();
+
+    public void Update()
+    {
+        colliderNames.Clear();
+        foreach (var collider in colliders)
+        {
+            colliderNames.Add(collider.gameObject.name);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        colliders.Add(collision.collider);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        colliders.Remove(collision.collider);
+    }
+}
