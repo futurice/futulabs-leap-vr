21e7b95aa75b84075c5ac1c8c23d613ed6d6dd3e
mathiassiig <mathias_sn@hotmail.com>
2017-09-21 13:30:47 +0300

    Cleaned up code in InstructionsManager

--- Assets/Scripts/InstructionsManager.cs
+++ Assets/Scripts/InstructionsManager.cs
@@ -8,11 +8,6 @@ namespace Futulabs
 {
 	public class InstructionsManager : MonoBehaviour 
 	{
-		[SerializeField] private List<InstructionCube> _instructionCubes = new List<InstructionCube>();
-		private int _currentCubeIndex = 0;
-		private float _timeToShow = 5f;
-		private bool _startTimer = true;
-		private int _lastCubeIndex = -1;
 		private Transform _user;
 		private InstructionCube _currentCube;
 
@@ -20,8 +15,6 @@ namespace Futulabs
 		{
 			_user = GameObject.FindGameObjectWithTag("MainCamera").transform;	
 		}
-
-		
 
 		void Update()
 		{
@@ -43,21 +36,6 @@ namespace Futulabs
 					}
 				}
 			}
-			// timer based:
-			/* 
-			if(_startTimer)
-			{
-				_startTimer = false;
-				if(_lastCubeIndex != -1) _instructionCubes[_lastCubeIndex].HideInstruction();
-				_instructionCubes[_currentCubeIndex].ShowInstruction();
-				_lastCubeIndex = _currentCubeIndex;
-				_currentCubeIndex = Mathf.RoundToInt(Mathf.Repeat(_currentCubeIndex+1,_instructionCubes.Count));
-				Debug.Log(_currentCubeIndex);
-				Observable.Timer(TimeSpan.FromSeconds(_timeToShow)).Subscribe(_ =>
-				{
-					_startTimer = true;
-				});
-			}*/
 		}
 	}
 }
