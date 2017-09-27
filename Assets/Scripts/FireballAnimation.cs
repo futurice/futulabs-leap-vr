fdde28a1c2e7dedda2ed5ff6409f905b041c9224
mathiassiig <mathias_sn@hotmail.com>
2017-09-27 16:40:55 +0300

    Removed old picking up animation, added fireball animation to instructions

---
+++ Assets/Scripts/FireballAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+namespace Futulabs
+{
+	public class FireballAnimation : MonoBehaviour 
+	{
+		[SerializeField] private Animator _animator;
+		[SerializeField] private ParticleSystem _particleSystem;
+
+		void Update()
+		{
+			var speed = _animator.GetFloat("speed");
+			if(speed < 1 && _particleSystem.gameObject.activeInHierarchy)
+			{
+				_particleSystem.gameObject.SetActive(false);
+			}
+			else if(speed == 1 && !_particleSystem.gameObject.activeInHierarchy)
+			{
+				_particleSystem.gameObject.SetActive(false);
+			}
+		}
+	}
+}
